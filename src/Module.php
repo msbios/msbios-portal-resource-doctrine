58d8727d1e55035f1c704f479f74266d563458c2
Judzhin Miles <info@msbios.com>
2019-05-23 17:25:06 +0300

    Lazy Composer MSBios Commit Script

--- src/Module.php
+++ src/Module.php
@@ -5,19 +5,11 @@
  */
 namespace MSBios\Portal\Resource\Doctrine;
 
-use Doctrine\DBAL\Platforms\MySqlPlatform;
-use Doctrine\ORM\EntityManager;
-use Zend\EventManager\EventInterface;
-use Zend\EventManager\LazyListenerAggregate;
-use Zend\ModuleManager\Feature\BootstrapListenerInterface;
-use Zend\Mvc\ApplicationInterface;
-use Zend\ServiceManager\ServiceLocatorInterface;
-
 /**
  * Class Module
  * @package MSBios\Portal\Resource\Doctrine
  */
-class Module extends \MSBios\Module implements BootstrapListenerInterface
+class Module extends \MSBios\Module
 {
     /** @const VERSION */
     const VERSION = '1.0.0';
@@ -38,33 +30,5 @@ class Module extends \MSBios\Module implements BootstrapListenerInterface
     protected function getNamespace()
     {
         return __NAMESPACE__;
-    }
-
-    /**
-     * @inheritdoc
-     *
-     * @param EventInterface $e
-     * @return array|void
-     * @throws \Doctrine\DBAL\DBALException
-     */
-    public function onBootstrap(EventInterface $e)
-    {
-        /** @var ApplicationInterface $target */
-        $target = $e->getTarget();
-
-        /** @var ServiceLocatorInterface $serviceManager */
-        $serviceManager = $target->getServiceManager();
-
-        /** @var MySqlPlatform $platform */
-        $platform = $serviceManager
-            ->get(EntityManager::class)
-            ->getConnection()
-            ->getDatabasePlatform();
-        $platform->registerDoctrineTypeMapping('enum', 'string');
-
-        (new LazyListenerAggregate(
-            $serviceManager->get(self::class)['listeners'],
-            $serviceManager
-        ))->attach($target->getEventManager());
     }
 }
